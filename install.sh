fe12feef48d81f80084f5db3e28f9649423e1648
v1xp <v1xp.ccox@gmail.com>
2026-06-24 05:29:38 -0400

    its all a test

--- install.sh
+++ install.sh
@@ -47,8 +47,7 @@ task_git() {
   git config --global user.name "v1XP.CCox"
 }
 first_inits() {
-  command -v nvim >/dev/null 2>&1 && nohup nvim --headless "+Lazy! sync" +qa >$logfile 2>&1
-  command -v nvim >/dev/null 2>&1 && nohup nvim --headless "MasonUpdate" +qa >$logfile 2>&1
+  command -v nvim >/dev/null 2>&1 && nohup nvim --headless "+Lazy! sync" +TSUpdate +qa >$logfile 2>&1
 }
 
 task_main() {
